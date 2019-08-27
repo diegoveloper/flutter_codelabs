43908c8b2c92f6e1abf334d01019e2a64d3d9d43
diegoveloper <diego.velasquez.lopez@gmail.com>
2019-08-27 11:13:39 -0500

    start from scratch

--- flutter_codelab_3/lib/main.dart
+++ flutter_codelab_3/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:sample_state_management/src/model/cart_model.dart';
 import 'src/screens/login_screen.dart';
 
 void main() => runApp(MyApp());
@@ -8,21 +6,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (_) => CartModel(),
-      child: MaterialApp(
-        theme: ThemeData.light().copyWith(
-          accentColor: Colors.black,
-          primaryColor: Colors.yellow,
-          buttonTheme: ButtonThemeData(
-            buttonColor: Colors.yellow,
-          ),
-          primaryIconTheme: IconThemeData(
-            color: Colors.black,
-          ),
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(
+        accentColor: Colors.black,
+        primaryColor: Colors.yellow,
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.yellow,
         ),
-        home: LoginScreen(),
+        primaryIconTheme: IconThemeData(
+          color: Colors.black,
+        ),
       ),
+      home: LoginScreen(),
     );
   }
 }
